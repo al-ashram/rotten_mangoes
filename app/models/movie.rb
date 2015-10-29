c28c4e54aa7953c3be77d44e479f72da67c1a449
sam-ash <sam.ashram@gmail.com>
2015-10-29 19:38:09 +0000

    upload image and url working

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,13 +1,19 @@
+# require 'carrierwave/orm/activerecord'
+
 class Movie < ActiveRecord::Base
 
   has_many :reviews
+
+  
 
   validates :title, presence: true
   validates :director, presence: true
   validates :runtime_in_minutes, numericality: { only_integer: true}
   validates :description, presence: true
-  validates :poster_image_url, presence: true
+  # validates :poster_image_url, presence: true
   validates :release_date, presence: true
+
+  mount_uploader :image, ImageUploader
 
   validate :release_date_is_not_in_the_future
 
